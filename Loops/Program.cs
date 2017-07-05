68d76e67d282f3c7300974d4d1ca1190654051a2
Your Name <you@example.com>
2017-07-05 15:15:33 -0400

    reviewed concepts

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -10,6 +10,18 @@ namespace Loops
     {
         static void Main(string[] args)
         {
+
+            // Nested loops
+            // Infinite loops
+            // Keywords break and continue
+
+            // For loop - can be used for both counting & iterating (or in other words, go to next one) through an array (or other collection type)
+            // for(initializer; condition; updater)
+            // {
+            //      do something
+            //  }
+
+
         }
     }
 }
