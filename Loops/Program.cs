540034eb372aa11cd7130688a030c75823d6fbfd
Your Name <you@example.com>
2017-07-06 10:11:00 -0400

    practice with for loops

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -16,10 +16,25 @@ namespace Loops
             // Keywords break and continue
 
             // For loop - can be used for both counting & iterating (or in other words, go to next one) through an array (or other collection type)
+
             // for(initializer; condition; updater)
             // {
             //      do something
             //  }
+
+            //for (int i = 15; i > 0; i--) // running i++ instead of i-- would be an infinite loop // people usually use i or counter
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            // counts from 50 to 100
+
+            for (int i = 50; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+
 
 
         }
